cf94c6500f8d817dcfaf627be0dcc92d2b64178f
parasa42914 <sheth.pr@somaiya.edu>
2020-11-21 20:11:44 +0530

    Revert "Backend | Added Swagger Documentation"
    This reverts commit 69570c4fb3fd41c2b18ecd4b4b232898f8267ad0.

--- backend/index.js
+++ backend/index.js
@@ -2,27 +2,12 @@ const express = require('express');
 const app = express();
 const bodyparser = require('body-parser');
 const mongoose = require('mongoose');
-const swaggerUi = require('swagger-ui-express');
-const swaggerJsdoc = require('swagger-jsdoc');
 const db = 'mongodb://localhost/kisaan';
 const port = 5000;
 
-const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Kisaan Bandhu',
-      description: 'An Api Documentation for Kisaan Bandhu',
-      version: '1.0.0',
-    },
-  },
-  apis: ['./routes/*.js'],
-};
-
-const swaggerSpec = swaggerJsdoc(options);
-
 const farmerLoginRoute = require('./routes/loginFarmer');
 const farmerRegisterRoute = require('./routes/registerFarmer');
+
 
 mongoose.connect(
   db,
@@ -39,9 +24,8 @@ mongoose.connect(
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use('/auth/farmer/login', farmerLoginRoute);
-app.use('/auth/farmer/register', farmerRegisterRoute);
+app.use('/auth/farmer/login',farmerLoginRoute);
+app.use('/auth/farmer/register',farmerRegisterRoute);
 
 app.use((req, res, next) => {
   const err = new Error('Not Found');
--- backend/routes/registerFarmer.js
+++ backend/routes/registerFarmer.js
@@ -2,49 +2,8 @@ const express = require('express');
 const Farmer = require('../models/farmerModel');
 const router = express.Router();
 
-/**
- * @swagger
- * /auth/farmer/register:
- *  post:
- *    summary: Farmer Register
- *    description: Register Api for farmer
- *    tags:
- *      - name: Farmer
- *    requestBody:
- *      content:
- *        application/json:
- *          schema:
- *            properties:
- *              mobile:
- *                type: number
- *              password:
- *                type: string
- *              name:
- *                type: string
- *              aadhar_no:
- *                type: number
- *              address:
- *                type: string
- *              latitude:
- *                type: number
- *              longitude:
- *                type: number
- *    responses:
- *      200:
- *        description: Farmer Register Successfully
- *      500:
- *        description: Error in Farmer Register API
- */
 router.post('/', (req, res) => {
-  const {
-    name,
-    password,
-    mobile,
-    aadhar_no,
-    address,
-    latitude,
-    longitude,
-  } = req.body;
+  const { name, password, mobile, aadhar_no, address, latitude, longitude } = req.body;
   const farmertemp = new Farmer({
     name,
     password,
@@ -52,7 +11,7 @@ router.post('/', (req, res) => {
     aadhar_no,
     address,
     latitude,
-    longitude,
+    longitude
   });
 
   farmertemp
@@ -65,9 +24,7 @@ router.post('/', (req, res) => {
       });
     })
     .catch((err) => {
-      res
-        .status(500)
-        .json({ error: 'Error in Farmer Register API', statusCode: 500 });
+      res.status(500).json({ error: 'Error in Farmer Register API', statusCode: 500 });
     });
 });
 
