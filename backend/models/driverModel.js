8b04f63673acf287fd5294f4f996f4ba9327d690
govinda1999 <gspatel61199@gmail.com>
2020-11-29 17:51:13 +0530

    Backend | Change in data type

--- backend/models/driverModel.js
+++ backend/models/driverModel.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const driverSchema = mongoose.Schema({
   name: { type: String, required: true },
-  mobile: { type: Number, required: true },
+  mobile: { type: String, required: true },
   address: { type: String, required: true },
-  latitude: { type: Number, required: true },
-  longitude: { type: Number, required: true },
-  capacity: { type: Number, required: true },
-  driverLicence: { type: Number, required: true },
+  latitude: { type: String, required: true },
+  longitude: { type: String, required: true },
+  capacity: { type: String, required: true },
+  driverLicence: { type: String, required: true },
 });
 
 module.exports = mongoose.model('driver', driverSchema);
--- backend/models/loginModel.js
+++ backend/models/loginModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const LoginSchema = mongoose.Schema({
 	password : {type:String,required:true},
-	mobile : { type: Number, required: true },
+	mobile : { type: String, required: true },
 	role : {type:String,required:true},
 	userFk: { type: mongoose.Schema.Types.ObjectId},
 });
