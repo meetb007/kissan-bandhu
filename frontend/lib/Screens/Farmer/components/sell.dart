403211886b9083911329f19ce9f7f7336a3b48de
meetb007 <meet.rb@somaiya.edu>
2020-12-12 15:50:05 +0530

    Frontend | Sell page image picker added.

--- frontend/lib/Screens/Farmer/components/sell.dart
+++ frontend/lib/Screens/Farmer/components/sell.dart
@@ -1,18 +1,42 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import '../bloc.navigation_bloc/navigation_bloc.dart';
+import 'package:image_picker/image_picker.dart';
 
- class Sell extends StatelessWidget with NavigationStates{
-  const Sell({
-    Key key
-  }) : super(key: key);
+class Sell extends StatefulWidget with NavigationStates{
+  @override
+  _SellState createState() => _SellState();
+}
 
-   @override
+class _SellState extends State<Sell> {
+  File _image;
+
+  Future getImage() async {
+    final image = await ImagePicker.pickImage(
+        source: ImageSource.camera, imageQuality: 50);
+    setState(() {
+      _image = image;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "Home / Sell Page",
-        style: TextStyle(fontWeight: FontWeight.w900, fontSize: 28),
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text('Sell'),
+      // ),
+      body: Center(
+          child: _image == null
+              ? Text("Image is not Loaded")
+              : Image.file(_image)),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Click',
+        child: Icon(Icons.camera_alt),
       ),
     );
   }
+
+
 }
