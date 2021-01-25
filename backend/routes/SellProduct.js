cd57e87ea49abea54b15796c06855ecccd8aaef4
govinda1999 <gspatel61199@gmail.com>
2021-01-25 15:31:31 +0530

    Backend | Working on History of Farmer

--- backend/routes/SellProduct.js
+++ backend/routes/SellProduct.js
@@ -47,42 +47,8 @@ const { name, type, quantity } = req.body;
 
 });
 
-router.get('/', async (req, res) => {
-  const token = req.headers.authorization;
-  if (!token) {
-    res.status(403).json({
-      message: 'Token is Required',
-      statusCode: 403,
-    });
-    return;
-  }
 
-  const farmer = await Token.findOne({ token }).exec();
-  if (!farmer || farmer.role != "farmer") {
-    res.status(403).json({
-      message: 'Token is Invalid',
-      statusCode: 403,
-    });
-    return ;
-  }
-
-
-  SellProduct
-    .find({userFk:farmer.userFk,status:"Placed"})
-    .then((doc) => {
-      res.status(200).json({
-        message: 'List of Product',
-        length:doc.length,
-        response: doc,
-        statusCode: 200,
-      });
-    })
-    .catch((err) => {
-      res
-        .status(500)
-        .json({ error: 'Error in Farmer Place Get API', statusCode: 500 });
-    });
-});
+//get List 
 
 router.get('/', async (req, res) => {
   const token = req.headers.authorization;
@@ -121,6 +87,45 @@ router.get('/', async (req, res) => {
     });
 });
 
+//get single 
+
+router.get('/:id', async (req, res) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    res.status(403).json({
+      message: 'Token is Required',
+      statusCode: 403,
+    });
+    return;
+  }
+
+  const farmer = await Token.findOne({ token }).exec();
+  if (!farmer || farmer.role != "farmer") {
+    res.status(403).json({
+      message: 'Token is Invalid',
+      statusCode: 403,
+    });
+    return ;
+  }
+
+
+  SellProduct
+    .findbyid(req.params.id)
+    .then((doc) => {
+      res.status(200).json({
+        message: 'Product',
+        response: doc,
+        statusCode: 200,
+      });
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ error: 'Error in Farmer Place Get Single API', statusCode: 500 });
+    });
+});
+
+//delete 
 router.delete('/', async (req, res) => {
   const token = req.headers.authorization;
   if (!token) {
@@ -157,6 +162,83 @@ router.delete('/', async (req, res) => {
         .json({ error: 'Error in Farmer Place Delete API', statusCode: 500 });
     });
 });
+
+// get history
+router.get('/history', async (req, res) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    res.status(403).json({
+      message: 'Token is Required',
+      statusCode: 403,
+    });
+    return;
+  }
+
+  const farmer = await Token.findOne({ token }).exec();
+  if (!farmer || farmer.role != "farmer") {
+    res.status(403).json({
+      message: 'Token is Invalid',
+      statusCode: 403,
+    });
+    return ;
+  }
+
+
+  SellProduct
+    .find({userFk:farmer.userFk,status:"Completed"})
+    .then((doc) => {
+      res.status(200).json({
+        message: 'List of Product',
+        length:doc.length,
+        response: doc,
+        statusCode: 200,
+      });
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ error: 'Error in Farmer Place Get History API', statusCode: 500 });
+    });
+});
+
+//get single history
+
+router.get('/history/:id', async (req, res) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    res.status(403).json({
+      message: 'Token is Required',
+      statusCode: 403,
+    });
+    return;
+  }
+
+  const farmer = await Token.findOne({ token }).exec();
+  if (!farmer || farmer.role != "farmer") {
+    res.status(403).json({
+      message: 'Token is Invalid',
+      statusCode: 403,
+    });
+    return ;
+  }
+
+
+  SellProduct
+    .findbyid(req.params.id)
+    .then((doc) => {
+      res.status(200).json({
+        message: 'Product',
+        response: doc,
+        statusCode: 200,
+      });
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ error: 'Error in Farmer Place Get Single History API', statusCode: 500 });
+    });
+});
+
 
 
 module.exports = router;
