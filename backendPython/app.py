328cd9146735adbc959475bc1e9065b35ffb13ad
govinda1999 <gspatel61199@gmail.com>
2021-02-01 16:53:11 +0530

    Backend | Working on PickUpLocation

--- backendPython/app.py
+++ backendPython/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from math import radians, cos, sin, asin, sqrt 
 
 app = Flask(__name__)
 
@@ -13,27 +14,49 @@ def knn():
 	data = jsonify(data)
 	farmers = data.farmers
 	driver = data.driver
-	pickUpNode = get_neighbors(farmers,driver,10)
-	return pickUpNode
+	#Threshold to 100 KM
+	threshold = 100
+	num_neighbors = 10
+	pickUpNodes = get_neighbors(farmers,driver,num_neighbors,threshold)
+	pickUpLocation = findPathForDriver(pickUpNodes)
+	return pickUpLocation
 
-# calculate the Euclidean distance between two vectors
-def euclidean_distance(row1, row2):
-	distance = 0.0
-	for i in range(len(row1)-1):
-		distance += (row1[i] - row2[i])**2
-	return sqrt(distance)
+# calculate the distance between two vectors
+def distanceBtwTwoLocation(driver,farmer):
+	lat1 = radians(driver[0])
+	lon1 = radians(driver[1])
+	lat2 = radians(farmer[0])
+	lon2 = radians(farmer[1])
+
+	#Haversine Formula
+	dlon = lon2 - lon1  
+	dlat = lat2 - lat1
+
+	a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+	c = 2 * asin(sqrt(a))
+	r = 6371
+	return (c * r)
+
 
 # Locate the most similar neighbors
-def get_neighbors(farmers, driver, num_neighbors):
+def get_neighbors(farmers, driver, num_neighbors,threshold):
 	distances = list()
 	for farmer in farmers:
-		dist = euclidean_distance(driver, farmer.coordinates)
-		distances.append((farmer, dist))
+		dist = distanceBtwTwoLocation(driver, farmer.coordinates)
+		if dist < threshold:
+			distances.append((farmer, dist))
+
 	distances.sort(key=lambda tup: tup[1])
 	neighbors = list()
 	for i in range(num_neighbors):
-		neighbors.append(distances[i][0])
+		if i < len(distances):
+			neighbors.append(distances[i][0])
 	return neighbors
+
+
+#Use Alogrithm to Find smallest path
+def findPathForDriver(pickUpNodes):
+	pass
 
 
 if __name__ == "__main__":
