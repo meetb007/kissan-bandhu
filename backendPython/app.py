9d0d94d081b6ddb83fa4ae724d6d3370da6ea02f
Govinda Patel <gspatel611999@gmail.com>
2021-02-23 14:37:34 +0530

    Backend | Working Deployment for Flask

--- backendPython/app.py
+++ backendPython/app.py
@@ -1,14 +1,27 @@
 from flask import Flask, request, jsonify
-from math import radians, cos, sin, asin, sqrt 
+from math import radians, cos, sin, asin, sqrt
+import os 
 import numpy as np
 from cv2 import cv2
 import json
+from keras.models import model_from_json
+import tensorflow.keras.optimizers as Optimizer
+from threading import Thread
+from werkzeug.utils import secure_filename
+
+uploadFolder = 'upload/'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = uploadFolder
+
+dictName = ['Rice','Tomato','Potato','LadyFinger','Strawberry']
 
 @app.route('/')
 def index():
-	return 'Flask API For Kissan Bandhu'
+	print("Call")
+	print(predictType("0.jpg"))
+	return jsonify(message='Hello From API',statusCode = 200)
 
 @app.route('/clustering',methods=["POST"])
 def knn():
@@ -26,8 +39,36 @@ def knn():
 
 @app.route('/predictItem',methods=["GET"])
 def predict():
-	return "predict"
+	if 'file' not in request.files:
+		return jsonify(message='Image is Required',statusCode = 304)
+	file = request.files['file']
+	if file.filename == '':
+		return jsonify(message='Select Proper Image',statusCode = 304)
+	if file and allowedFile(file.filename):
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config(['UPLOAD_FOLDER'],filename)))
+		return jsonify(data=predictType(filename),statusCode = 200)
 	
+
+def allowedFile(filename):
+	return "." in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
+
+def predictType(filename):
+	print("Hello")
+	jsonFile = open('demo.json','r')
+	loadedModelJson = jsonFile.read()
+	print("Hello12")
+	jsonFile.close()
+	loadedModel = model_from_json(loadedModelJson,custom_objects=None)
+	loadedModel.load_weights("model.h5")
+	loadedModel.compile(optimizer=Optimizer.Adam(lr=0.0001),loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+	print('Hello3')
+	print('upload/' + filename)
+	img = cv2.imread("upload/" + filename)
+	img = cv2.resize(img,(150,150))
+	predictArray = loadedModel.predict(np.array([img]))[0]
+	index = np.where(predictArray == max(predictArray))[0][0]
+	return dictName[index]
 
 
 # calculate the distance between two vectors
