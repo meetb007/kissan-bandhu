480048e2dbdade51b9fd381bafc54c35e1890b17
Govinda Patel <gspatel611999@gmail.com>
2021-02-28 19:47:40 +0530

    Backend | Write Schema for Cart and Orders

---
+++ backend/models/orderModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Product = require('./sellProductModel');
+const Buyer = require('./buyerModel');
+
+const ordersSchema = mongoose.Schema({
+  date: { type: Date, default: new Date() },
+  orderId: { type: String, required: true },
+  status: { type: String },
+  payment: { type: String, required: true },
+  product: { type: mongoose.Schema.Types.ObjectId, ref: Product, required: true },
+  buyer: { type: mongoose.Schema.Types.ObjectId, ref: Buyer, required: true },
+  payment_mode: { type: String, required: true },
+  payment_done: { type: String, required: true },
+});
+
+module.exports = mongoose.model('orders', ordersSchema);
